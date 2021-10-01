32dee2ebb6009f641b5d116c2bf2e1f8f94bc492
Isack Ramirez <isack.ramirez1@gmail.com>
2021-09-30 20:29:16 -0500

    deleting elistincashe

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 export default class Navbar extends Component {
-  state = {}
+
+
+  state = {activeItem:'Blue'}
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -11,7 +13,7 @@ export default class Navbar extends Component {
 
     return (
         <div>
-      <Menu stackable>
+      <Menu stackable inverted>
         <Menu.Item>
           {/* <img src='/logo.png' /> */}
         </Menu.Item>
@@ -20,6 +22,8 @@ export default class Navbar extends Component {
           name='build'
           active={activeItem === 'build'}
           onClick={this.handleItemClick}
+          color={'blue'}
+          
         >
           Build a Deck
         </Menu.Item>
@@ -28,6 +32,7 @@ export default class Navbar extends Component {
           name='view'
           active={activeItem === 'view'}
           onClick={this.handleItemClick}
+          color={'blue'}
         >
           View all Decks
         </Menu.Item>
@@ -36,6 +41,7 @@ export default class Navbar extends Component {
           name='browse'
           active={activeItem === 'browse'}
           onClick={this.handleItemClick}
+          color={'blue'}
         >
           Browse all Cards
         </Menu.Item>
@@ -44,6 +50,7 @@ export default class Navbar extends Component {
           name='profile'
           active={activeItem === 'profile'}
           onClick={this.handleItemClick}
+          color={'blue'}
         >
           Profile
         </Menu.Item>
