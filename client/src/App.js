b3c500f00d37998fb8ec26390b0b0238fde5a7f8
Arnold Lee <arnoldlee60@gmail.com>
2021-10-05 16:34:16 -0500

    inital heroku

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as HashRouter, Switch, Route } from "react-router-dom";
 import Splash from "./components/Splash"
-import Test from "./components/Test"
+import Test from "./components/test"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div>
       
       <HashRouter basename={process.env.PUBLIC_URL}>
+
       <Navbar />
           <Route exact path="/" component={Splash} />
           <Route exact path="/Test" component={Test} />
-         
-      
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
       </HashRouter>
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,9 @@ import { Menu, Input } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import { NavLink, withRouter } from "react-router-dom";
 import { BrowserRouter as HashRouter, Switch, Route } from "react-router-dom";
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+
 export default class Navbar extends Component {
   state = { activeItem: "Blue" };
 
@@ -118,7 +121,7 @@ export default class Navbar extends Component {
 
         <Menu.Item //show login and signup if NOT logged in
           name='Login'
-
+          linkButton={true} href="/login"
           active={activeItem === 'Login'}
           onClick={this.handleItemClick}
           color={'blue'}
