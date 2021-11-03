4b23404be1ab29b596503a9f47869646065d39d6
unknown <marcodotr@gmail.coom>
2021-11-03 13:21:20 -0500

    messing up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,7 @@
 // hello
 
 import React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
- import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from "./components/Navbar";
-import Home from './pages/Home';
-import Signup from './components/Signup';
-import Login from './components/Login';
+
 import HeroSelect from './components/HeroSelect'
 
 // Construct our main GraphQL API endpoint
